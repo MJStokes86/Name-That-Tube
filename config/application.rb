5def1d9d326e0d5863ecbdc63b5bc63f2a708d71
Michael Stokes <mjstokes1986@att.net>
2015-09-01 18:12:37 -0400

    update google fonts url

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,9 @@ module NameThatTube
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.paths << "#{Rails.root}/app/assets/Videos"
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
+
 
 
   end
